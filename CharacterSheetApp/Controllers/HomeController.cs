916908fc6ac207068438ff92f25197c10b817be6
Bradley Anderson <bcaytonanderson@gmail.com>
2016-10-11 13:48:54 -0500

    Added functionality to the Index method in the HomeController.

--- CharacterSheetApp/Controllers/HomeController.cs
+++ CharacterSheetApp/Controllers/HomeController.cs
@@ -1,13 +1,19 @@
 using Microsoft.AspNetCore.Mvc;
 using CharacterSheetApp.Models;
 
+// the name of our app
 namespace CharacterSheetApp.Controllers
 {
+	// class
 	public class HomeController : Controller
 	{
+		// method
+		// this method returns an IActionResult, making it an Action Method.
 		public IActionResult Index()
 		{
-			return View();
+			var name = "Hux";
+			// we will pass this var to our View as a parameter
+			return View("Index", name);
 		}
 	}
 }
