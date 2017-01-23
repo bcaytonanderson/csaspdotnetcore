7136c3a60dcf123e12f5fc06932233c08345d14c
Bradley Anderson <bcaytonanderson@gmail.com>
2017-01-23 15:17:39 -0600

    Defined a Create method in HomeController to add characters to list.

--- CharacterSheetApp/Controllers/HomeController.cs
+++ CharacterSheetApp/Controllers/HomeController.cs
@@ -5,8 +5,19 @@ namespace CharacterSheetApp.Controllers
 {
 	public class HomeController : Controller
 	{
-		var model = new CharacterSheetApp.Models.Character();
-		model.Name = "Hux";
-		return View(model);
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		public IActionResult Create(string characterName)
+		{
+
+			var model = new CharacterSheetApp.Models.Character();
+			model.Name = CharacterName;
+
+			// pass the name of the Index view as a parameter to redirect to the index after the model is created.
+			return View("Index",model);
+		}
 	}
 }
