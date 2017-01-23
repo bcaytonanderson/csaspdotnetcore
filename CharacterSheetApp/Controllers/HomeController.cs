9f6db5dbc5c82cc54994e3c7f2553d323e9a501a
Bradley Anderson <bcaytonanderson@gmail.com>
2017-01-23 14:54:07 -0600

    Fixed error in controller.

--- CharacterSheetApp/Controllers/HomeController.cs
+++ CharacterSheetApp/Controllers/HomeController.cs
@@ -5,8 +5,11 @@ namespace CharacterSheetApp.Controllers
 {
 	public class HomeController : Controller
 	{
+		public IActionResult Index()
+		{
 		var model = new CharacterSheetApp.Models.Character();
 		model.Name = "Hux";
 		return View(model);
+		}
 	}
 }
