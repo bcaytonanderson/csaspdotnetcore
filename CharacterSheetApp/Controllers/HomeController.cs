c88391bc982fda6997f2a56d43ead846997f4a39
Bradley Anderson <bcaytonanderson@gmail.com>
2017-01-23 14:30:23 -0600

    Unit 1, part 3. Set up character model; creating a new Character object from the controller; accessing Character data from the view.

--- CharacterSheetApp/Controllers/HomeController.cs
+++ CharacterSheetApp/Controllers/HomeController.cs
@@ -8,15 +8,21 @@ namespace CharacterSheetApp.Controllers
 	public class HomeController : Controller
 	{
 		// method
-		// this method returns an IActionResult, making it an Action Method.
+			// this method returns an IActionResult, making it an Action Method.
 
-		// the method "Index" tells the controller to search for a view called "index.cshtml"
-		
-		public IActionResult Index()
-		{
-			var name = "Hux";
-			// we will pass this var to our View as a parameter
-			return View("Index", name);
-		}
+			// the method "Index" tells the controller to search for a view called "index.cshtml"
+			// public IActionResult Index()
+			// {
+			// 	var name = "Hux";
+			// 	// we will pass this var to our View as a parameter
+			// 	return View("Index", name);
+			// }
+
+		// once we have some functionality in the Model, we will set the model to a Character object.
+
+		// since the Character model lives outside of the controller, we need to enter the full namespace and class to access it.
+		var model = new CharacterSheetApp.Models.Character();
+		model.Name = "Hux";
+		return View(model);
 	}
 }
---
+++ CharacterSheetApp/Models/Character.cs
@@ -0,0 +1,9 @@
+using System.Collections.Generic;
+
+namespace CharacterSheetApp.Models
+{
+	public class Character
+	{
+		public string Name;
+	}
+}
