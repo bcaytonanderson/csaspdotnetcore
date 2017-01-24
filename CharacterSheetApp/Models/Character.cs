77aba5cfd48745107290322b4f84803f9327876b
Bradley Anderson <bcaytonanderson@gmail.com>
2017-01-23 18:05:00 -0600

    Lesson 2, Unit 2, part 2. Using the asp-for tag helper and creating a data annotation for rendering the name of a property in a specific way.

--- CharacterSheetApp/Models/Character.cs
+++ CharacterSheetApp/Models/Character.cs
@@ -6,6 +6,9 @@ namespace CharacterSheetApp.Models
 	public class Character
 	{
 		public string Name { get; set; }
+
+		// any time the name of IsActive, it will display with a space as we have written here
+		[Display(Name = "Is Active")]
 		public bool IsActive  { get; set; }
 		public int Level  { get; set; }
 		public int Strength { get; set; }
